dde01a4d78e3bc7d6738922f6f683f6f39e0e384
Kayuan165 <kayuandiasoli@gmail.com>
2024-06-22 16:03:33 -0300

    Adicionado uma nova biblioteca para auxiliar na API (cors)

--- Server.js
+++ Server.js
@@ -1,11 +1,13 @@
 import express, { json, request, response } from "express";
 import { PrismaClient } from '@prisma/client'
+import cors from 'cors'
+
 
 const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-
+app.use(cors())
 
 app.post('/usuarios', async (req, res) => {
 
